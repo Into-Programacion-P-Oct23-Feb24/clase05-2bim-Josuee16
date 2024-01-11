0b3e8b505b0e57dec0a5171be246936c3ab3bf7f
Josuee16 <147664232+Josuee16@users.noreply.github.com>
2024-01-11 15:51:51 -0500

    Cambios Clase

---
+++ EjemplosRecursividad/src/paquete03/Ejemplo01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author VEL-USER
+ */
+public class Ejemplo01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String[][] obtenerArreglo() {
+        String[][] arreglo = {{"Loja", "Cuenca", "Esmeraldas"},
+        {"Quito", "Zamora", "Machala"}};
+        return arreglo;
+    }
+}
