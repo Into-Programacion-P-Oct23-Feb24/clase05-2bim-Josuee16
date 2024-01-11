5226d82f4d280827e6ca0261695bacc67b5cfc44
Josuee16 <147664232+Josuee16@users.noreply.github.com>
2024-01-11 17:52:47 -0500

    Cambios clase

--- EjemplosRecursividad/src/paquete02/Ejemplo08.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo08.java
@@ -3,14 +3,25 @@
  * 5! = 5 * 4 * 3 * 2 * 1
  */
 package paquete02;
-
+import java.util.Scanner;
 public class Ejemplo08 {
 
     public static void main(String[] args) {
         // Ingresar por teclado el tamaño de un arreglo
         // Ingresar por teclado los valores enteros del arreglo
         // Sumar los valores del arreglo, mediante la función misterio
-        
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("Define el tamaño del arreglo");
+        int n = entrada.nextInt();
+        int [] arreglo = new int[n];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese los datos del arreglo");
+            arreglo[i] = entrada.nextInt(); 
+        }
+        for (int i = 0; i < arreglo.length; i++) {
+            misterio(arreglo,n);
+        }
+        System.out.println(misterio(arreglo,arreglo.length));
     }
 
     public static int misterio(int[] arreglo, int tamanio) {
